656d6330ae809497307e6fa7f3f7e42408126f78
Shawf-Lee <li823311847@gmail.com>
2020-09-18 09:39:44 +0800

    题号47

--- src/main/java/com/shawf/Permute.java
+++ src/main/java/com/shawf/Permute.java
@@ -20,9 +20,22 @@ import java.util.List;
  * [3,1,2],
  * [3,2,1]
  * ]
+ *
+ * 47. 全排列 II
+    给定一个可包含重复数字的序列，返回所有不重复的全排列。
+    示例:
+    输入: [1,1,2]
+    输出:
+    [
+    [1,1,2],
+    [1,2,1],
+    [2,1,1]
+    ]
  */
 public class Permute {
     private List<List<Integer>> res;
+    private List<List<Integer>> res1;
+    private boolean[] visit;
 
     public List<List<Integer>> permute(int[] nums) {
         res = new ArrayList<>();
@@ -31,7 +44,7 @@ public class Permute {
         }
         List<Integer> list = new ArrayList<>();
         int[] visit = new int[nums.length];
-        dfs(nums,visit , list);
+        dfs(nums, visit, list);
         return res;
     }
 
@@ -41,7 +54,7 @@ public class Permute {
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if(visit[i] == 1) continue;
+            if (visit[i] == 1) continue;
             list.add(nums[i]);
             visit[i] = 1;
             dfs(nums, visit, list);
@@ -50,9 +63,47 @@ public class Permute {
         }
     }
 
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        res1 = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        visit = new boolean[nums.length];
+        Arrays.sort(nums);
+        dfs2(nums, list);
+        return res1;
+    }
+
+    public void dfs2(int[] nums, List<Integer> list) {
+        if (list.size() == nums.length) {
+            res1.add(new ArrayList(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visit[i]) {
+                continue;
+            }
+
+            //升序排序后，剪掉前一个元素等于当前元素的，!visit[i-1] 与 visit[i-1] 剪枝区别与更彻底问题见题解
+            if (i > 0 && !visit[i - 1] && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            list.add(nums[i]);
+            visit[i] = true;
+            dfs2(nums, list);
+            visit[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        List<List<Integer>> res1 = new Permute().permute(new int[]{1, 2, 3});
+        Permute permute = new Permute();
+        List<List<Integer>> res1 = permute.permute(new int[]{1, 2, 3});
+        List<List<Integer>> res2 = permute.permuteUnique(new int[]{1, 1, 2});
+        System.out.println("46.全排列Ⅰ：");
         for (List<Integer> list : res1) {
+            System.out.println(Arrays.toString(list.toArray()));
+        }
+        System.out.println("47.全排列Ⅱ：");
+        for (List<Integer> list : res2) {
             System.out.println(Arrays.toString(list.toArray()));
         }
     }
